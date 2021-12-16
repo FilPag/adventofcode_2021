31c7a925b07d210f40b2f9d04b2b62b491d92ec1
FilPag <filip.pagliaro.4567@student.uu.se>
2021-12-16 19:43:42 +0100

    16

--- 16/2.py
+++ 16/2.py
@@ -1,26 +1,21 @@
+import math
 def read_literal(sequence):
   value , sequence= trim_n_bits(sequence, 5)
   leading = value[0]
   value = value[1:]
 
-  counter = 12
   while leading != "0":
-    counter += 5
-
     next_group, sequence = trim_n_bits(sequence, 5)
     leading = next_group[0]
     next_group = next_group[1:]
     value += next_group
   
-  remainder = counter % 4
-  #_, sequence = trim_n_bits(sequence, remainder)
   return int(value, base=2), sequence
 
 
 def trim_n_bits(sequence, n):
   trim =  sequence[:n]
   sequence =  sequence[n:]
-
   return trim, sequence
 
 def read_header(sequence):
@@ -29,7 +24,6 @@ def read_header(sequence):
   
   version = int(version, base=2)
   type_id= int(type_id, base=2)
-
   return version, type_id, sequence
 
 def expand_char(c):
@@ -37,38 +31,66 @@ def expand_char(c):
   string = str(bin(int_value))[2:]
   return string.zfill(4)
 
-def process_packet(sequence):
-  v, tid, sequence =  read_header(sequence)
-
-  if tid == 4:
-    value, sequence = read_literal(sequence)
-  else:
-    mode, sequence = trim_n_bits(sequence, 1)
-
-    if mode == '0':
+def process_sub(mode, sequence):
+  values = []
+  if mode == '0':
       no_bits, sequence = trim_n_bits(sequence, 15)
       no_bits = int(no_bits, base=2)
       subs, sequence = trim_n_bits(sequence, no_bits)
       while subs != "":
         sub_v, subs = process_packet(subs)
-        v += sub_v
-    else:
-      no_packets, sequence = trim_n_bits(sequence, 11)
-      no_packets = int(no_packets, base=2)
-      for _ in range(no_packets):
-        sub_v, sequence = process_packet(sequence)
-        v += sub_v
-  return v, sequence
+        values.append(sub_v)
+  else:
+    no_packets, sequence = trim_n_bits(sequence, 11)
+    no_packets = int(no_packets, base=2)
+    for _ in range(no_packets):
+      sub_v, sequence = process_packet(sequence)
+      values.append(sub_v)
+  return values, sequence
 
+def process_packet(sequence):
+  _, tid, sequence =  read_header(sequence)
+  if tid == 4:
+    return read_literal(sequence)
+
+  mode, sequence = trim_n_bits(sequence, 1)
+  res = 0
+  vals, seq = process_sub(mode, sequence)
+  match tid:
+    case 0:
+      res = sum(vals)
+    case 1:
+      res = math.prod(vals)
+    case 2:
+      res = min(vals)
+    case 3:
+      res = max(vals)
+    case 5:
+      if vals[0] > vals[1]:
+        res = 1
+      else:
+        res = 0
+    case 6:
+      if vals[0] < vals[1]:
+        res = 1
+      else:
+        res = 0
+    case 7:
+      if vals[0] == vals[1]:
+        res = 1
+      else:
+        res = 0
+  return res, seq
 
 if __name__ == "__main__":
   file = open("input.txt", "r")
 
   sequence = ""
   line = file.readline().rstrip()
+  file.close()
   for c in line:
     sequence += expand_char(c)
   
-  tot_version = process_packet(sequence)
-  print(tot_version)
-  file.close()
+  
+  tot_value = process_packet(sequence)[0]
+  print(tot_value)
